a54051964974bf275fb3981baeb213702c9f1e47
Pavel Kilko <pavel.kilko@gmail.com>
2025-02-18 20:33:16 +0200

    feat: basic setup

---
+++ kub-backend/src/main/java/education/kub/backend/KubBackendApplication.java
@@ -0,0 +1,13 @@
+package education.kub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubBackendApplication.class, args);
+    }
+
+}
---
+++ kub-backend/src/test/java/education/kub/backend/KubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package education.kub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
